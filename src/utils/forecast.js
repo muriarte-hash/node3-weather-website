4d5d296cbc2099e62b2c0ba5e3c966390350dd00
muriarte <muriarte@muriartes-MacBook-Pro.local>
2025-08-10 19:46:21 -0600

    Add high and low to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -10,7 +10,8 @@ const forecast = (latitude, longitude, callback) => {
             callback('Unable to find location', undefined)
 
         } else {
-            callback(undefined, body.current.weather_descriptions[0] + ". It is currently " + body.current.temperature + " degress out.")
+    
+            callback(undefined, body.current.weather_descriptions[0] + ". It is currently " + body.current.temperature + " degress out. The humidty today is " + body.current.humidity + "With a wind speed of " + body.current.wind_speed)
 
 
         }
